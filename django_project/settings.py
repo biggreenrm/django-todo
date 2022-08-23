4b4cbfb775a3ea3d81fe05215541cc14e1c1b10c
Sergey Gurkov <biggreen.rm@gmail.com>
2022-08-23 10:46:54 +0300

    [IMP] main: base functionality (detailview, listview) with tests

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,9 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party
+    'rest_framework',
     # local
     'todos.apps.TodosConfig',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.todo = Todo.objects.create(
+            title='title',
+            body='body'
+        )
+    
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "title")
+        self.assertEqual(self.todo.body, "body")
+        self.assertEqual(str(self.todo), "title")
+
+class TodoViewsTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.todo = Todo.objects.create(
+            title='title',
+            body='body'
+        )
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), len(response.json()))
+        self.assertContains(response, self.todo)
+    
+    def test_api_detailview(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}),
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, "title")
